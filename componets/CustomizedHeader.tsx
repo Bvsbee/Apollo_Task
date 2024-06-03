f660da9c7da82aad6c8bc3c5bb18782919814cca
Bvsbee <151123082+Bvsbee@users.noreply.github.com>
2024-06-03 04:57:50 -0700

    Customized Drawer Nav

---
+++ componets/CustomizedHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
+import { View, Text,TouchableOpacity } from 'react-native'
+export default function customizedHeader() {
+  return (
+    
+  )
+}
