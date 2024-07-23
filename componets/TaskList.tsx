7208e1735f4fa9bfbf312e026f7e0bbb92a937a5
Michael Busbee <busbee.michael101@gmail.com>
2024-07-23 13:43:04 -0700

    added remove and add button for task

--- componets/TaskList.tsx
+++ componets/TaskList.tsx
@@ -1,12 +1,51 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 //What does a Task consist of? Priority, Name, Due by, Desc,due date, notes?
 
+
+const
+
+const TASK = [
+  {
+    header: "Task",
+    items: [{}],
+  },
+];
+
 export default function TaskList() {
   return (
-    <View>
-      <Text>TaskList</Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <TouchableOpacity activeOpacity={0.8}>
+          <View style={styles.addIcon}>
+            <Ionicons name="add-circle-sharp" size={35} color="#4a90e2" />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.8}>
+          <View style={styles.removeIcon}>
+            <FontAwesome5 name="minus-circle" size={35} color="#DC143C" />
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row: {
+    flexDirection: "row",
+    position: "absolute",
+    alignItems: "center",
+    right: 30,
+    bottom: -750,
+  },
+  addIcon: {},
+  removeIcon: {},
+});
--- screens/TabScreens/ToDoScreen.tsx
+++ screens/TabScreens/ToDoScreen.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import TaskList from "../../componets/TaskList";
 
 export default function ToDoScreen() {
   return (
     <View>
-      <Text></Text>
+      <TaskList />
     </View>
   );
 }
