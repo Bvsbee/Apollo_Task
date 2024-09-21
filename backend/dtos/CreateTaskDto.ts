47bd069631d9b37f1d820752269880e6a38f7b9b
Michael Busbee <busbee.michael101@gmail.com>
2024-09-21 12:39:18 -0700

    Added class vaildator and validated createTaskDto

--- backend/dtos/CreateTaskDto.ts
+++ backend/dtos/CreateTaskDto.ts
@@ -1,13 +1,36 @@
+import {
+  IsBoolean,
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 import { PriorityEnum } from 'task/TaskPriority-enum';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   id: string;
 
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
+  @IsEnum(PriorityEnum, { message: 'Priority must be High Medium or Low' })
   priority: PriorityEnum;
 
+  @IsString()
+  @IsNotEmpty()
+  @IsDate()
   dueDate: string;
 
-  desc: string;
+  @IsOptional()
+  @IsString()
+  desc?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isCompleted: boolean;
 }
