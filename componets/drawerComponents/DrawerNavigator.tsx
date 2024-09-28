80479548b71ebb23e4b2dcef2a32b863ce5ac3eb
Bvsbee <151123082+Bvsbee@users.noreply.github.com>
2024-09-27 19:16:43 -0700

    Removed screen and imports that were no longer being used.

--- componets/drawerComponents/DrawerNavigator.tsx
+++ componets/drawerComponents/DrawerNavigator.tsx
@@ -1,37 +1,24 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { SettingsScreen } from "../../screens/SettingsScreens";
-import {
-  ApolloScreen,
-  CalenderScreen,
-  ToDoScreen,
-} from "../../screens/TabScreens";
+import { CalenderScreen, ToDoScreen } from "../../screens/TabScreens";
 import ToDoHeader from "../screenHeaders/ToDoHeader";
 import ShoppingListScreen from "../../screens/TabScreens/ShoppingListScreen";
 import CreateList from "../shoppingListComponents/CreateList";
 
 const Drawer = createDrawerNavigator();
 
-
 export default function DrawerNavigator() {
   return (
     <Drawer.Navigator
       screenOptions={{
         headerStyle: {
-          borderBottomColor: "black",
           backgroundColor: "#f0f8ff",
           borderWidth: 1,
           borderColor: "#4a90e2",
         },
       }}
     >
-      {/*  <Drawer.Screen
-        name="AquariusAI"
-        component={ApolloScreen}
-        options={({}) => ({
-          headerRight: () => <TestFile />,
-        })}
-      /> */}
       <Drawer.Screen
         name="To-Do List"
         component={ToDoScreen}
