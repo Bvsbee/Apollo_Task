9d44044ab71940cb6cca722d1bedffca7efaccd3
Bvsbee <151123082+Bvsbee@users.noreply.github.com>
2024-09-27 20:52:34 -0700

    Created comments to describe what's going on

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import "react-native-gesture-handler";
 import DrawerNavigator from "./componets/drawerComponents/DrawerNavigator";
 import { TaskProvider } from "./context/TaskProvider";
 
+// App Entry point
+// Task Provider wraps app allowing global task management
+
 export default function App() {
   return (
     <TaskProvider>
