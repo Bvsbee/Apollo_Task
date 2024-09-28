3c4e781753494724a90146fcd1e7893e925fdda9
Michael Busbee <busbee.michael101@gmail.com>
2024-09-28 15:21:23 -0700

    Made Comment change

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import DrawerNavigator from "./componets/drawerComponents/DrawerNavigator";
 import { TaskProvider } from "./context/TaskProvider";
 
 // App Entry point
-// Task Provider wraps app allowing global task management
+// Task Provider wraps app allowing global task management via React Context API
 
 export default function App() {
   return (
