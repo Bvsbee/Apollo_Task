430cc6450e3acacbcbd69ed8e9a42f8cacadcbe2
Bvsbee <151123082+Bvsbee@users.noreply.github.com>
2024-10-31 21:58:45 -0700

    Changed styles

--- screens/login/Login.tsx
+++ screens/login/Login.tsx
@@ -52,9 +52,9 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#f0f0f5",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f0f0f5",
   },
   formContainer: {
     padding: 20,
